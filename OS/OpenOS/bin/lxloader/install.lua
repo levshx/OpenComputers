48e11082fd1fefad08ea2c3b4744bf2337a5a0f6
Pavel Levshic <pavellevshic@gmail.com>
2021-01-26 00:32:47 +0300

    Update install.lua

--- OS/OpenOS/bin/lxloader/install.lua
+++ OS/OpenOS/bin/lxloader/install.lua
@@ -2,10 +2,13 @@ local shell = require("shell")
 local fs = require("filesystem")
 local term = require("term")
 
+term.clear()
+
 fs.makeDirectory("/usr/lib")
 if fs.exists("/usr/lib/lx.lua") then
   fs.remove("/usr/lib/lx.lua")
 end
+
 
 -- start
 
@@ -21,21 +24,25 @@ shell.execute("wget -f https://raw.githubusercontent.com/levshx/OpenComputers/ma
 os.sleep(4)
 
 -- make Dirictory /etc/lx/
+term.clear()
 print("MAKE LX DIRICTORIES  ")
 fs.makeDirectory("/etc/lx")
 os.sleep(1)
 
 -- Download rep CONFIG
+term.clear()
 print("GET REPOSITORY CONFIG")
 shell.execute("wget -f URL /etc/lx/rep_list")
 os.sleep(2)
 
 -- Download standart rep MANIFEST
+term.clear()
 print("GET STANDART APP LIST")
 shell.execute("lx cache")
 os.sleep(4)
 
 -- finish
+term.clear()
 print("Complete")
 os.sleep(0.5)
 print("lx app was installed")
