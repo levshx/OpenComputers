41e489349619a3600732eea6899f97ad04875d2e
Pavel Levshic <pavellevshic@gmail.com>
2021-01-25 22:03:22 +0300

    Create main.lua

---
+++ OS/OpenOS/lib/aspect/main.lua
@@ -0,0 +1,421 @@
+local aspect = {
+ 
+    -- Tier 0
+    aer = {
+        label = "Aer",
+        name = "gaseousaeressentia",
+        tier = 0,
+        foreground = 0x080808,
+        background = 0xffff00,
+    },
+    terra = {
+        label = "Terra",
+        name = "gaseousterraessentia",
+        tier = 0,
+        foreground = 0xffffff,
+        background = 0x5faf00
+    },
+    aqua = {
+        label = "Aqua",
+        name = "gaseousaquaessentia",
+        tier = 0,
+        foreground = 0xffffff,
+        background = 0x5fafff
+    },
+    ignis = {
+        label = "Ignis",
+        name = "gaseousignisessentia",
+        tier = 0,
+        foreground = 0x080808,
+        background = 0xff5f00
+    },
+    ordo = {
+        label = "Ordo",
+        name = "gaseousordoessentia",
+        tier = 0,
+        foreground = 0x080808,
+        background = 0xd7d7d7
+    },
+    perditio = {
+        label = "Perditio",
+        name = "gaseousperditioessentia",
+        tier = 0,
+        foreground = 0xffffff,
+        background = 0x5f5f5f
+    },
+ 
+    -- Tier 1
+ 
+    gelum = {
+        label = "Gelum",
+        name = "gaseousgelumessentia",
+        tier = 1,
+        foreground = 0x080808,
+        background = 0xafd7d7
+    },
+    lux = {
+        label = "Lux",
+        name = "gaseousluxessentia",
+        tier = 1,
+        foreground = 0x080808,
+        background = 0xd7d700
+    },
+    motus = {
+        label = "Motus",
+        name = "gaseousmotusessentia",
+        tier = 1,
+        foreground = 0x080808,
+        background = 0xafafaf
+    },
+    permutatio = {
+        label = "Permutatio",
+        name = "gaseouspermutatioessentia",
+        tier = 1,
+        foreground = 0xffffff,
+        background = 0x5f8787
+    },
+    potentia = {
+        label = "Potentia",
+        name = "gaseouspotentiaessentia",
+        tier = 1,
+        foreground = 0x080808,
+        background = 0xafd7d7
+    },
+    tempestas = {
+        label = "Tempestas",
+        name = "",
+        tier = 1,
+        foreground = 0x080808,
+        background = 0xeeeeee
+    },
+    vacuos = {
+        label = "Vacuos",
+        name = "gaseousvacuosessentia",
+        tier = 1,
+        foreground = 0x080808,
+        background = 0xa8a8a8
+    },
+    venenum = {
+        label = "Venenum",
+        name = "gaseousvenenumessentia",
+        tier = 1,
+        foreground = 0x080808,
+        background = 0x87d700
+    },
+    victus = {
+        label = "Victus",
+        name = "gaseousvictusessentia",
+        tier = 1,
+        foreground = 0xffffff,
+        background = 0xaf0000
+    },
+    vitreus = {
+        label = "Vitreus",
+        name = "gaseousvitreusessentia",
+        tier = 1,
+        foreground = 0x080808,
+        background = 0xafffff
+    },
+ 
+    -- Tier 2
+ 
+    bestia = {
+        label = "Bestia",
+        name = "gaseousbestiaessentia",
+        tier = 2,
+        foreground = 0xffffff,
+        background = 0x875f00
+    },
+    fames = {
+        label = "Fames",
+        name = "gaseousfamesessentia",
+        tier = 2,
+        foreground = 0xffffff,
+        background = 0x870000
+    },
+    herba = {
+        label = "Herba",
+        name = "gaseousherbaessentia",
+        tier = 2,
+        foreground = 0xffffff,
+        background = 0x008700
+    },
+    iter = {
+        label = "Iter",
+        name = "gaseousiteressentia",
+        tier = 2,
+        foreground = 0xffffff,
+        background = 0xd75f87
+    },
+    limus = {
+        label = "Limus",
+        name = "gaseouslimusessentia",
+        tier = 2,
+        foreground = 0x585858,
+        background = 0x87d700
+    },
+    metallum = {
+        label = "Metallum",
+        name = "gaseousmetallumessentia",
+        tier = 2,
+        foreground = 0x585858,
+        background = 0xafafd7
+    },
+    mortuus = {
+        label = "Mortuus",
+        name = "gaseousmortuusessentia",
+        tier = 2,
+        foreground = 0xffffff,
+        background = 0xaf8787
+    },
+    praecantatio = {
+        label = "Praecantatio",
+        name = "gaseouspraecantatioessentia",
+        tier = 2,
+        foreground = 0xffffff,
+        background = 0xaf00ff
+    },
+    sano = {
+        label = "Sano",
+        name = "gaseoussanoessentia",
+        tier = 2,
+        foreground = 0xffffff,
+        background = 0xff0000
+    },
+    tenebrae = {
+        label = "Tenebrae",
+        name = "gaseoustenebraeessentia",
+        tier = 2,
+        foreground = 0xffffff,
+        background = 0x444444
+    },
+    vinculum = {
+        label = "Vinculum",
+        name = "gaseousvinculumessentia",
+        tier = 2,
+        foreground = 0xffffff,
+        background = 0x875f5f
+    },
+    volatus = {
+        label = "Volatus",
+        name = "gaseousvolatusessentia",
+        tier = 2,
+        foreground = 0x585858,
+        background = 0xd7d7af
+    },
+    tempus = {
+        label = "Tempus",
+        name = "gaseoustempusessentia",
+        tier = 2,
+        foreground = 0x080808,
+        background = 0xd7afff
+    },
+ 
+    -- Tier 3
+ 
+    alienis = {
+        label = "Alienis",
+        name = "gaseousalienisessentia",
+        tier = 3,
+        foreground = 0xffffff,
+        background = 0xaf5f87
+    },
+    arbor = {
+        label = "Arbor",
+        name = "gaseousarboressentia",
+        tier = 3,
+        foreground = 0xffffff,
+        background = 0xaf8700
+    },
+    auram = {
+        label = "Auram",
+        name = "gaseousauramessentia",
+        tier = 3,
+        foreground = 0x080808,
+        background = 0xd7afd7
+    },
+    corpus = {
+        label = "Corpus",
+        name = "gaseouscorpusessentia",
+        tier = 3,
+        foreground = 0xffffff,
+        background = 0xd75f87
+    },
+
+    exanimis = {
+        label = "Exanimis",
+        name = "gaseousexanimisessentia",
+        tier = 3,
+        foreground = 0xffffff,
+        background = 0x5f5f00
+    },
+    spiritus = {
+        label = "Spiritus",
+        name = "gaseousspiritusessentia",
+        tier = 3,
+        foreground = 0xffffff,
+        background = 0x6c6c6c
+    },
+    vitium = {
+        label = "Vitium",
+        name = "gaseousvitiumessentia",
+        tier = 3,
+        foreground = 0xffffff,
+        background = 0xaf0087
+    },
+ 
+    -- Tier 4
+ 
+    cognitio = {
+        label = "Cognitio",
+        name = "gaseouscognitioessentia",
+        tier = 4,
+        foreground = 0x080808,
+        background = 0xd7afaf
+    },
+    sensus = {
+        label = "Sensus",
+        name = "gaseoussensusessentia",
+        tier = 4,
+        foreground = 0x4e4e4e,
+        background = 0x87d7ff
+    },
+ 
+    -- Tier 5
+ 
+    humanus = {
+        label = "Humanus",
+        name = "gaseoushumanusessentia",
+        tier = 5,
+        foreground = 0x4e4e4e,
+        background = 0xffd7af
+    },
+ 
+    -- Tier 6
+ 
+    instrumentum = {
+        label = "Instrumentum",
+        name = "gaseousinstrumentumessentia",
+        tier = 6,
+        foreground = 0xffffff,
+        background = 0x5f5fd7
+    },
+    lucrum = {
+        label = "Lucrum",
+        name = "gaseouslucrumessentia",
+        tier = 6,
+        foreground = 0xffffff,
+        background = 0xd7af00
+    },
+    messis = {
+        label = "Messis",
+        name = "gaseousmessisessentia",
+        tier = 6,
+        foreground = 0x4e4e4e,
+        background = 0xd7af87
+    },
+    perfodio = {
+        label = "Perfodio",
+        name = "gaseousperfodioessentia",
+        tier = 6,
+        foreground = 0x4e4e4e,
+        background = 0xd7d7d7
+    },
+ 
+    -- Tier 7
+ 
+    fabrico = {
+        label = "Fabrico",
+        name = "gaseousfabricoessentia",
+        tier = 7,
+        foreground = 0x4e4e4e,
+        background = 0x87afaf
+    },
+    machina = {
+        label = "Machina",
+        name = "gaseousmachinaessentia",
+        tier = 7,
+        foreground = 0xffffff,
+        background = 0x8787af
+    },
+    meto = {
+        label = "Meto",
+        name = "gaseousmetoessentia",
+        tier = 7,
+        foreground = 0xffffff,
+        background = 0xd7af5f
+    },
+    pannus = {
+        label = "Pannus",
+        name = "gaseouspannusessentia",
+        tier = 7,
+        foreground = 0x4e4e4e,
+        background = 0xffffd7
+    },
+    telum = {
+        label = "Telum",
+        name = "gaseoustelumessentia",
+        tier = 7,
+        foreground = 0xffffff,
+        background = 0xd75f5f
+    },
+    tutamen = {
+        label = "Tutamen",
+        name = "gaseoustutamenessentia",
+        tier = 7,
+        foreground = 0xffffff,
+        background = 0x87d7d7
+    },
+}
+ 
+aspect.gelum.parents = {aspect.ignis, aspect.perditio}
+aspect.lux.parents = {aspect.aer, aspect.ignis }
+aspect.motus.parents = {aspect.aer, aspect.ordo }
+aspect.permutatio.parents = {aspect.ordo, aspect.perditio }
+aspect.potentia.parents = {aspect.ignis, aspect.ordo }
+aspect.tempestas.parents = {aspect.aer, aspect.aqua }
+aspect.vacuos.parents = {aspect.aer, aspect.perditio }
+aspect.venenum.parents = {aspect.aqua, aspect.perditio }
+aspect.victus.parents = {aspect.aqua, aspect.terra }
+aspect.vitreus.parents = {aspect.ordo, aspect.terra }
+ 
+aspect.bestia.parents = {aspect.motus, aspect.victus }
+aspect.fames.parents = {aspect.vacuos, aspect.victus }
+aspect.herba.parents = {aspect.terra, aspect.victus }
+aspect.iter.parents = {aspect.motus, aspect.terra }
+aspect.limus.parents = {aspect.aqua, aspect.victus }
+aspect.metallum.parents = {aspect.terra, aspect.vitreus }
+aspect.mortuus.parents = {aspect.perditio, aspect.victus }
+aspect.praecantatio.parents = {aspect.potentia, aspect.vacuos }
+aspect.sano.parents = {aspect.ordo, aspect.victus }
+aspect.tenebrae.parents = {aspect.lux, aspect.vacuos }
+aspect.vinculum.parents = {aspect.motus, aspect.perditio }
+aspect.volatus.parents = {aspect.aer, aspect.motus }
+aspect.tempus.parents = {aspect.vacuos, aspect.ordo}
+ 
+aspect.alienis.parents = {aspect.tenebrae, aspect.vacuos }
+aspect.arbor.parents = {aspect.aer, aspect.herba }
+aspect.auram.parents = {aspect.aer, aspect.praecantatio }
+aspect.corpus.parents = {aspect.bestia, aspect.mortuus }
+aspect.exanimis.parents = {aspect.mortuus, aspect.motus }
+aspect.spiritus.parents = {aspect.mortuus, aspect.victus }
+aspect.vitium.parents = {aspect.perditio, aspect.praecantatio }
+ 
+aspect.cognitio.parents = {aspect.ignis, aspect.spiritus }
+aspect.sensus.parents = {aspect.aer, aspect.spiritus }
+ 
+aspect.humanus.parents = {aspect.bestia, aspect.cognitio }
+ 
+aspect.instrumentum.parents = {aspect.humanus, aspect.ordo }
+aspect.lucrum.parents = {aspect.fames, aspect.humanus }
+aspect.messis.parents = {aspect.herba, aspect.humanus }
+aspect.perfodio.parents = {aspect.humanus, aspect.terra }
+ 
+aspect.fabrico.parents = {aspect.humanus, aspect.instrumentum }
+aspect.machina.parents = {aspect.instrumentum, aspect.motus }
+aspect.meto.parents = {aspect.instrumentum, aspect.messis }
+aspect.pannus.parents = {aspect.bestia, aspect.instrumentum }
+aspect.telum.parents = {aspect.instrumentum, aspect.ignis }
+aspect.tutamen.parents = {aspect.instrumentum, aspect.terra }
+ 
+return aspect
