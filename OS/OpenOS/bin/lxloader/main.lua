fc7885553a6b80259068e73c53ff579e510547cb
Pavel Levshic <pavellevshic@gmail.com>
2021-01-25 23:33:57 +0300

    Update main.lua

--- OS/OpenOS/bin/lxloader/main.lua
+++ OS/OpenOS/bin/lxloader/main.lua
@@ -8,20 +8,20 @@ local shell = require("shell")
 local local args, options = shell.parse(...)
 
 function help()
-  local text = [[lx help                 -- get all commands
- lx cache                -- update cache (manifest program list)
- lx apps                 -- listen all installed apps
- lx install App_name     -- install app by app name
- lx del App_name         -- remove app by app name from computer
- lx update App_name      -- update app by app name
- lx update               -- update all apps
- lx info App_name        -- get all info by app name
- lx libs                 -- listen all installed libs 
- lx getlib Lib_name      -- get lib by lib name         
- lx dellib Lib_name      -- remove lib by lib name from computer
- lx reps                 -- listen all used reps
- lx addrep MANIFEST_url  -- add new repository by MANIFEST table URL
- lx delrep MANIFEST_url  -- add new repository by MANIFEST table URL]]
+  local text = [[  lx help                 -- get all commands
+  lx cache                -- update cache (manifest program list)
+  lx apps                 -- listen all installed apps
+  lx install App_name     -- install app by app name
+  lx del App_name         -- remove app by app name from computer
+  lx update App_name      -- update app by app name
+  lx update               -- update all apps
+  lx info App_name        -- get all info by app name
+  lx libs                 -- listen all installed libs 
+  lx getlib Lib_name      -- get lib by lib name         
+  lx dellib Lib_name      -- remove lib by lib name from computer
+  lx reps                 -- listen all used reps
+  lx addrep MANIFEST_url  -- add new repository by MANIFEST table URL
+  lx delrep MANIFEST_url  -- add new repository by MANIFEST table URL]]
   print(text)  
 end
 
