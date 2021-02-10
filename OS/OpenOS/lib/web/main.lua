c58d485ec588fbff416e1a84b0817ae3f2a53585
Pavel Levshic <pavellevshic@gmail.com>
2021-02-10 22:02:07 +0300

    Update main.lua

--- OS/OpenOS/lib/web/main.lua
+++ OS/OpenOS/lib/web/main.lua
@@ -1,12 +1,15 @@
 -- lib by levshx
 -- web.get(url,headers) - WEB METHOD GET (return result)
 -- web.post(url,dataTable,headers) - WEB METHOD POST (return result)
+-- web.debug - WEB DEBUG OUTPUT PARAM (false default)
 
 local web = {}
 local internet = require("internet")
 local url
 local postData = {}
 local headers 
+
+web.debug = false
 
 function webError(err)             
   print("lib web.lua Fucking web error: "..err)
